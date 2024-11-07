abfa85aa3380181f2b01677b62b3b96151e83e88
Taejin <taerattanakun@gmail.com>
2024-11-07 13:53:49 +0700

    Edit-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,82 @@
-import { useState } from 'react'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import HomeAllProducts from './pages/HomeAllProducts'
+import ProductPage from './pages/ProductPage'
+import CartPage from './pages/CartPage'
+import CheckOutPage from './components/CheckOutPage'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import ChangePassword from './pages/ChangePassword'
+import ResetPassword from './pages/ResetPassword'
+import UserProfile from './pages/UserProfile'
+import UserEditProfile from './pages/UserEditProfile'
+import Admin from './pages/Admin'
+import Layout from './Layouts/Layout'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/homeallproducts',
+        element: <HomeAllProducts />
+      },
+      {
+        path: '/productpage',
+        element: <ProductPage />
+      },
+      {
+        path: '/cartpage',
+        element: <CartPage />
+      },
+      {
+        path: '/checkoutpage',
+        element: <CheckOutPage />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <SignUp />
+      },
+      {
+        path: '/changepassword',
+        element: <ChangePassword />
+      },
+      {
+        path: '/resetpassword',
+        element: <ResetPassword />
+      },
+      {
+        path: '/userprofile',
+        element: <UserProfile />
+      },
+      {
+        path: '/usereditprofile',
+        element: <UserEditProfile />
+      },
+      {
+        path: '/admin',
+        element: <Admin />
+      },
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>Hello World</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
