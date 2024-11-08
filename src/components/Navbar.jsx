2b8cc12f07f632ed1d8027ab19737338f8a97240
rawisut06 <tungtung0685@gmail.com>
2024-11-08 11:41:10 +0700

    updated nav and reponsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,40 @@
 import logo from "../assets/logo.svg";
+import logoIcon from "../assets/logo-icon.svg";
+import logoName from "../assets/Handy Haven.svg";
 import search from "../assets/search.svg";
 import login from "../assets/login.svg";
 import cart from "../assets/cart.svg";
+import burger from "../assets/burger.svg";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="w-full h-[167px] bg-[#4A4947] text-white">
-      <div className="flex items-center flex-col h-full justify-between py-[16px] px-[32px]">
-        <div className="flex relative justify-between items-center w-full">
-          <div className="flex items-center justify-center flex-grow">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="flex absolute right-0">
-            <img className="w-[35px] h-[35px]" src={search} alt="search-icon" />
-            <img className="w-[35px] h-[35px]" src={login} alt="login-icon" />
-            <img className="w-[35px] h-[35px]" src={cart} alt="cart-icon" />
-          </div>
+    <div className="w-full h-full bg-[#4A4947] text-white ">
+      <div className="flex items-center h-fit justify-between px-[32px] py-[16px] relative max-lg:px-[12px]">
+        <div className="flex items-center justify-center max-lg:hidden">
+          <img className="h-[48px]" src={logo} alt="logo" />
         </div>
-
-        <nav>
-          <ul className="flex gap-4">
-            <Link to='/homeallproducts'>All Product</Link>
-            <li>Hone Decor</li>
-            <li>Bath & Body</li>
-            <li>Apparel</li>
-            <li>Accessories</li>
+        <div className="flex space-x-2 lg:hidden">
+          <img className="h-[48px]" src={logoIcon} alt="logo-icon" />
+          <img className="sm-hidden" src={logoName} alt="handy-heaven" />
+        </div>
+        <nav className="absolute transform -translate-x-1/2 left-1/2 max-md:hidden">
+          <ul className="flex items-center space-x-6 text-center">
+            <Link className="hover:underline hover:decoration-solid" to='/homeallproducts'>All Product</Link>
+            <li className="hover:underline hover:decoration-solid">Home Decor</li>
+            <li className="hover:underline hover:decoration-solid">Bath & Body</li>
+            <li className="hover:underline hover:decoration-solid">Apparel</li>
+            <li className="hover:underline hover:decoration-solid">Accessories</li>
           </ul>
         </nav>
+        <div className="flex space-x-4 max-md:absolute max-md:transform max-md:-translate-x-1/2 max-md:left-1/2">
+          <img className="w-[24px] h-[24px]" src={search} alt="search-icon" />
+          <img className="w-[24px] h-[24px]" src={login} alt="login-icon" />
+          <img className="w-[24px] h-[24px]" src={cart} alt="cart-icon" />
+        </div>
+        <div className="md:hidden">
+          <img src={burger} alt="burger-icon" />
+        </div>
       </div>
     </div>
   )
