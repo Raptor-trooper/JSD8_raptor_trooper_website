d5246ae608b4301d22c39b93163bff9bc134b1d8
Rawisut06 <tungtung0685@gmail.com>
2024-12-04 09:50:49 +0700

    edited responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ const items = [
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(items[0]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const data = useContext(DataCategory);
+  const { category } = useContext(DataCategory);
 
   const handleSelected = (id) => {
     setSelectedItem(items.find((item) => item.id === id));
@@ -41,22 +41,22 @@ const Home = () => {
 
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? data.length - 1 : prevIndex - 1);
+      prevIndex === 0 ? category.length - 1 : prevIndex - 1);
   }
 
   const handleNextClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === data.length - 1 ? 0 : prevIndex + 1);
+      prevIndex === category.length - 1 ? 0 : prevIndex + 1);
   }
 
   const getWrappedIndex = (index) => {
-    return ((index % data.length) + data.length) % data.length;
+    return ((index % category.length) + category.length) % category.length;
   };
 
   return (
     <div className='flex flex-col items-center text-black bg-[#FAF7F0]'>
       {/* ส่วนลดสูงสุด 50% */}
-      <div className='flex justify-center w-full h-screen'>
+      <div className='flex justify-center w-full md:h-screen h-fit'>
         <div className='flex flex-col md:flex-row items-center p-[84px] mx-auto max-w-[1440px]'>
           <div className='mb-8 md:mr-8 md:mb-0'>
             <h1 className='text-5xl font-bold md:text-7xl'>Up to 50% Off</h1>
@@ -79,58 +79,58 @@ const Home = () => {
       </div>
 
       {/* ส่วนหัวข้อและรูปภาพ */}
-      <div className='flex justify-center w-full overflow-hidden'>
+      <div className='flex justify-center w-full md:overflow-hidden'>
 
-        <div className="flex flex-col p-[84px] max-w-[1280px]">
+        <div className="flex flex-col px-4 py-8 p-[84px] mx-auto max-w-[1280px] md:px-12">
           {/* ส่วนหัวข้อ */}
-          <h1 className="text-[96px] font-bold mb-8">Category</h1>
+          <h1 className="text-[96px] max-md:text-center font-bold mb-8">Category</h1>
 
-          <div className='relative flex flex-row items-center gap-12 mx-auto'>
+          <div className='flex flex-col items-center gap-4 mx-auto md:flex-row md:gap-12 md:relative'>
 
+            {/* ข้อความโปรโมทสินค้า */}
+            <div className='md:absolute top-0 left-1/2 max-w-[400px] md:translate-x-[70px] transform lg:-translate-x-[12%]'>
+                <p className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+
+          <div className='flex flex-row items-center gap-4 max-md:p-[16px]'>
             {/* รูปแรก */}
-            <div className='relative w-[700px] h-[700px] flex-none'>
+            <div className='relative lg:w-[700px] lg:h-[700px] w-[70vw] h-[70vw] flex-none'>
               <img
                 className="object-cover w-full h-full rounded-lg"
-                src={data[currentIndex]?.image}
+                src={category[currentIndex]?.image}
                 alt= ""
               />
               <div className="absolute bottom-0 flex items-center bg-[#FAF7F0] w-fit h-fit bg-opacity-60">
-                <h2 className='py-3 pl-4 pr-24 text-2xl'>{data[getWrappedIndex(currentIndex)]?.category}</h2>
+                <h2 className='py-3 pl-4 pr-24 text-2xl'>{category[getWrappedIndex(currentIndex)]?.category}</h2>
               </div>
             </div>
 
             {/* รูปสอง */}
-            <div className='w-[400px] h-[400px] relative flex-none'>
+            <div className='lg:w-[400px] lg:h-[400px] w-[40vw] h-[40vw] relative flex-none max-md:hidden'>
               <img
                 className="object-cover w-full h-full rounded-lg"
-                src={data[getWrappedIndex(currentIndex + 1)]?.image}
+                src={category[getWrappedIndex(currentIndex + 1)]?.image}
                 alt= ""
               />
               <div className="absolute bottom-0 flex items-center bg-[#FAF7F0] w-fit h-fit bg-opacity-60">
-                <h2 className='py-3 pl-4 pr-24 text-2xl'>{data[getWrappedIndex(currentIndex + 1)]?.category}</h2>
+                <h2 className='py-3 pl-4 pr-24 text-2xl'>{category[getWrappedIndex(currentIndex + 1)]?.category}</h2>
               </div>
             </div>
             {/* รูปสาม */}
-            <div className='w-[400px] h-[400px] relative flex-none'>
+            <div className='lg:w-[400px] lg:h-[400px] w-[40vw] h-[40vw] relative flex-none max-lg:hidden'>
               <img
                 className="object-cover w-full h-full rounded-lg"
-                src={data[getWrappedIndex(currentIndex + 2)]?.image}
+                src={category[getWrappedIndex(currentIndex + 2)]?.image}
                 alt= ""
               />
               <div className="absolute bottom-0 flex items-center bg-[#FAF7F0] w-fit h-fit bg-opacity-60">
-                <h2 className='py-3 pl-4 pr-24 text-2xl'>{data[getWrappedIndex(currentIndex + 2)]?.category}</h2>
+                <h2 className='py-3 pl-4 pr-24 text-2xl'>{category[getWrappedIndex(currentIndex + 2)]?.category}</h2>
               </div>
             </div>
-
-
-
-              {/* ข้อความโปรโมทสินค้า */}
-              <div className='absolute top-0 left-1/2 max-w-[400px] transform -translate-x-[12%]'>
-                <p className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
+          </div>
 
               {/* ปุ่มเลื่อนรูป */}
-              <div className='absolute bottom-0 left-1/2 w-[400px] transform -translate-x-[12%]'>
+              <div className='md:absolute p-4 bottom-0 md:left-1/2 lg:w-[400px] md:w-[40vw] w-full transform lg:-translate-x-[12%] md:translate-x-[70px]'>
                 <div className='flex justify-between w-full'>
                   <button onClick={handlePrevClick} className='text-black border-black btn btn-outline btn-circle'> ❮ </button>
                   <button onClick={handleNextClick} className='text-black border-black btn btn-outline btn-circle'> ❯ </button>
@@ -155,20 +155,20 @@ const Home = () => {
 
       {/* ส่วนเลือกสินค้า/รูป ตามปุ่ม */}
       <div className='flex justify-center w-full'>
-        <div className='p-[84px] h-screen flex flex-col gap-4 max-w-[1280px] mx-auto'>
-          <h1 className='w-full font-bold text-center text-[96px] md:text-left md:text-7xl'>Shop by room</h1>
+        <div className='md:p-[84px] p-[16px] h-fit flex flex-col items-center gap-4 md:max-w-[1280px] w-full mx-auto'>
+          <h1 className='w-fit font-bold text-center text-[96px] md:text-left md:text-7xl'>Shop by room</h1>
           {/* mobile */}
-          <div className='flex w-full py-4 space-x-4 md:hidden carousel carousel-center'>
+          <div className='w-5/6 p-6 space-x-2 md:hidden carousel carousel-center'>
             {/* card */}
             {items.map((item) => (
-              <div key={item.id} className='carousel-item h-[460px] w-[300px] text-center justify-between flex flex-col'>
+              <div key={item.id} className='flex flex-col items-center gap-4 text-center w-fit h-fit carousel-item'>
                 <img
-                  className="object-cover w-full rounded-lg h-3/4"
+                  className="object-cover rounded-lg"
                   src={item.img}
-                  alt="img-container"
+                  alt="item-image"
                 />
                 <p className='text-lg'>{item.name}</p>
-                <button className='px-12 py-3 text-white bg-black'>Explore more</button>
+                <button className='w-full px-12 py-3 text-white bg-black'>Explore more</button>
               </div>
             ))}
           </div>
