3508eea486dea8b98a1e1a5d8f913d34ac684ee6
Ben Taikam <bentaikam@Bens-MacBook-Air.local>
2024-12-06 12:02:17 +0700

    add admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 const Admin = () => {
     return (
-        <div>Admin</div>
-    )
-}
+        <div className="flex h-screen">
+            {/* Sidebar Section */}
+            <Sidebar />
 
-export default Admin
+            {/* Main Admin Content Section */}
+            <div className="flex-1 p-6">
+                <h1 className="text-4xl font-bold mb-6">Admin Dashboard</h1>
+                {/* Add your main admin content here */}
+                <div>
+                    Welcome to the Admin Dashboard!
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Admin;
