5fbc8da448bd2c552f3a94327eb2344b746dd9bc
rawisut06 <tungtung0685@gmail.com>
2024-12-14 22:51:40 +0700

    fixed conflict

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -139,169 +139,13 @@ const CheckoutPage = () => {
       }));
     };
 
-    return (
-        <form
-          onSubmit={onSubmitHandler}
-          className="grid max-w-screen-xl gap-8 p-4 mx-auto lg:grid-cols-3"
-        >
-          {/* ----------- Delivery Information ----------- */}
-          {/* <div className="space-y-4 lg:col-span-2">
-            <h2 className="text-2xl font-bold">Delivery Information</h2>
-            <div className="grid gap-4 md:grid-cols-2">
-            <div>
-              <h3 className="text-lg font-semibold">First Name</h3>
-              <input
-                type="text"
-                name="firstName"
-                value={firstName}
-                onChange={handleChange}
-                className="w-full p-2 mt-2 border rounded"
-                />
-            </div>
-              <input
-                required
-                name="lastName"
-                value={lastName}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded"
-                type="text"
-                placeholder="Last Name"
-              />
-            </div> */}
-            {/* <input
-              required
-              name="email"
-              value={email}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded"
-              type="email"
-              placeholder="Email Address"
-            /> */}
-            {/* <input
-              required
-              name="address"
-              value={address}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded"
-              type="text"
-              placeholder="Address"
-            />
-            <div className="grid gap-4 md:grid-cols-3">
-              <input
-                required
-                name="city"
-                value={city}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded"
-                type="text"
-                placeholder="City"
-              /> */}
-              {/* <input
-                name="state"
-                value={state}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded"
-                type="text"
-                placeholder="State"
-              /> */}
-              {/* <input
-                required
-                name="zip"
-                value={zip}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded"
-                type="text"
-                placeholder="Zipcode"
-=======
-  const { Api, cartItems, setCartItems, getCartAmount, category, token } = useContext(ShopContext);
-  const [cartData, setCartData] = useState([]);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    street: "",
-    city: "",
-    state: "",
-    zipcode: "",
-    country: "",
-    phone: "",
-  });
-
-  const onChangeHandler = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setFormData((data) => ({ ...data, [name]: value }));
-  };
-
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
-
-    try {
-      let orderItems = [];
-
-      for (const items in cartItems) {
-        if (cartItems[items] > 0) {
-          const itemInfo = structuredClone(
-            category.find((product) => product._id === items)
-          );
-          if (itemInfo) {
-            itemInfo.quantity = cartItems[items];
-            orderItems.push(itemInfo);
-          }
-        }
-      }
-
-      let orderData = {
-        delivery: formData,
-        items: orderItems,
-        amount: getCartAmount() + 10,
-      };
-
-      const responseStripe = await axios.post(
-        `${Api}/order/stripe`,
-        orderData,
-        { headers: { authorization: `Bearer ${token}` } }
-      );
-      await axios.post(
-        `${Api}/user/userprofile`,
-        { delivery: formData },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      if (responseStripe.data.success) {
-        const { session_url } = responseStripe.data;
-        window.location.replace(session_url);
-      } else {
-        console.log(error);
-        // toast.error(responseStripe.data.message);
-      }
-    } catch (error) {
-      console.log(error);
-      // toast.error(error.message);
-    }
-  };
-
-  useEffect(() => {
-    if (category.length > 0) {
-      const tempData = [];
-      for (const items in cartItems) {
-        if (cartItems[items] > 0) {
-          tempData.push({
-            _id: items,
-            quantity: cartItems[items],
-          });
-        }
-      }
-      setCartData(tempData);
-    }
-  }, [cartItems, category]);
-
   return (
     <form
       onSubmit={onSubmitHandler}
-      className="max-w-screen-xl mx-auto p-4 grid gap-8 lg:grid-cols-3"
+      className="grid max-w-screen-xl gap-8 p-4 mx-auto lg:grid-cols-3"
     >
       {/* ----------- Delivery Information ----------- */}
-      <div className="lg:col-span-2 space-y-4">
+      {/* <div className="space-y-4 lg:col-span-2">
         <h2 className="text-2xl font-bold">Delivery Information</h2>
         <div className="grid gap-4 md:grid-cols-2">
           <input
@@ -309,7 +153,7 @@ const CheckoutPage = () => {
             name="firstName"
             value={formData.firstName}
             onChange={onChangeHandler}
-            className="border border-gray-300 rounded py-2 px-3 w-full"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
             type="text"
             placeholder="First Name"
           />
@@ -318,7 +162,7 @@ const CheckoutPage = () => {
             name="lastName"
             value={formData.lastName}
             onChange={onChangeHandler}
-            className="border border-gray-300 rounded py-2 px-3 w-full"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
             type="text"
             placeholder="Last Name"
           />
@@ -328,7 +172,7 @@ const CheckoutPage = () => {
           name="email"
           value={formData.email}
           onChange={onChangeHandler}
-          className="border border-gray-300 rounded py-2 px-3 w-full"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
           type="email"
           placeholder="Email Address"
         />
@@ -337,7 +181,7 @@ const CheckoutPage = () => {
           name="street"
           value={formData.street}
           onChange={onChangeHandler}
-          className="border border-gray-300 rounded py-2 px-3 w-full"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
           type="text"
           placeholder="Street Address"
         />
@@ -347,7 +191,7 @@ const CheckoutPage = () => {
             name="city"
             value={formData.city}
             onChange={onChangeHandler}
-            className="border border-gray-300 rounded py-2 px-3 w-full"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
             type="text"
             placeholder="City"
           />
@@ -355,7 +199,7 @@ const CheckoutPage = () => {
             name="state"
             value={formData.state}
             onChange={onChangeHandler}
-            className="border border-gray-300 rounded py-2 px-3 w-full"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
             type="text"
             placeholder="State"
           />
@@ -364,7 +208,7 @@ const CheckoutPage = () => {
             name="zipcode"
             value={formData.zipcode}
             onChange={onChangeHandler}
-            className="border border-gray-300 rounded py-2 px-3 w-full"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
             type="text"
             placeholder="Zipcode"
           />
@@ -374,7 +218,7 @@ const CheckoutPage = () => {
           name="country"
           value={formData.country}
           onChange={onChangeHandler}
-          className="border border-gray-300 rounded py-2 px-3 w-full"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
           type="text"
           placeholder="Country"
         />
@@ -383,45 +227,15 @@ const CheckoutPage = () => {
           name="phone"
           value={formData.phone}
           onChange={onChangeHandler}
-          className="border border-gray-300 rounded py-2 px-3 w-full"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
           type="text"
           placeholder="Phone Number"
-        />
-      </div>
+              />
+            </div> */}
 
-      {/* ----------- Order Summary ----------- */}
-      <div className="space-y-4">
-        <h2 className="text-2xl font-bold">Order Summary</h2>
-        {cartData.map((item, index) => {
-          const product = category.find((cate) => cate._id === item._id);
-          return (
-            <div key={index} className="flex items-center gap-4">
-              <img
-                src={product.image[0]}
-                alt={product.name}
-                className="w-16 h-16 object-cover rounded"
-            <input
-              required
-              name="country"
-              value={country}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded"
-              type="text"
-              placeholder="Country"
-            />
-            <input
-              required
-              name="phone"
-              value={phone}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded"
-              type="text"
-              placeholder="Phone Number"
-            />
-          </div> */}
-          {/* Profile Section */}
-        <div className="space-y-4 lg:col-span-2">
-          <div className="grid grid-cols-2 gap-8">
+      {/* Profile Section */}
+      <div className="space-y-4 lg:col-span-2">
+        <div className="grid grid-cols-2 gap-8">
             <div>
               <h3 className="text-lg font-semibold">First Name</h3>
                 <input
@@ -443,10 +257,10 @@ const CheckoutPage = () => {
                 className="w-full p-2 mt-2 border rounded"
               />
             </div>
-          </div>
+        </div>
 
-          {/* Address Section */}
-          <div className="space-y-4">
+        {/* Address Section */}
+        <div className="space-y-4">
             <div>
               <h3 className="text-lg font-semibold">Country/Region</h3>
                 <input
@@ -490,7 +304,6 @@ const CheckoutPage = () => {
                   className="w-full p-2 mt-2 border rounded"
                 />
               </div>
-
             </div>
 
             <div>
@@ -503,14 +316,14 @@ const CheckoutPage = () => {
                 className="w-full p-2 mt-2 border rounded"
               />
             </div>
-          </div>
         </div>
+      </div>
 
-          {/* ----------- Order Summary ----------- */}
-          <div className="space-y-4">
-            <h2 className="text-2xl font-bold">Order Summary</h2>
-            {cartData.map((item, index) => {
-              const product = category.find((cate) => cate._id === item._id);
+      {/* ----------- Order Summary ----------- */}
+      <div className="space-y-4">
+        <h2 className="text-2xl font-bold">Order Summary</h2>
+          {cartData.map((item, index) => {
+            const product = category.find((cate) => cate._id === item._id);
               return (
                 <div key={index} className="flex items-center gap-4">
                   <img
@@ -546,10 +359,9 @@ const CheckoutPage = () => {
             >
               Confirm & Pay
             </button>
-          </div>
-        </form>
-    );
+    </div>
+    </form>
+  );
 };
 
 export default CheckoutPage;
-
