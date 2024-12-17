d5340a53e77feb961dedd91bd1b25f9859f081e6
rawisut06 <tungtung0685@gmail.com>
2024-12-18 00:26:24 +0700

    edit protect route

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
       {
         path: '/admin/*',
         element:
-        <ProtectRoute requiedRoles={['admin']}>
+        <ProtectRoute>
           <Admin />
         </ProtectRoute>
       },
